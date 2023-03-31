271591495d7e0def353c10cc2befe389b06edfce
DAAN <daanmgneni@gmail.com>
2023-04-01 04:00:51 +0530

    Add:Print Max

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-namespace GenreicDemo
+﻿namespace GenreicDemo
 {
 
     public class Program
@@ -10,23 +7,21 @@ namespace GenreicDemo
         {
             int[] valuesInt = { 1, 2, 3, 4, 5 };
             double[] valuesDouble = { 10.4, 6.7, 8.2, 9.5, 7.3 };
-            string[] valuesString = { "ram", "shiv", "radha" };
+            string[] valuesString = { "ram", "shiv", "radha", "mohan" };
 
             FindMax<int> obj = new FindMax<int>(valuesInt);
-            int MaxInt = obj.TestMaximum();
-            Console.WriteLine("Maximum value in integer is: " + MaxInt);
+            obj.PrintMax();
             Console.WriteLine("**********************************************");
 
 
             FindMax<double> obj1 = new FindMax<double>(valuesDouble);
-            double MaxDouble = obj1.TestMaximum();
-            Console.WriteLine("Maximum value in double is: " + MaxDouble);
+            obj1.PrintMax();
             Console.WriteLine("**********************************************");
 
             FindMax<string> obj2 = new FindMax<string>(valuesString);
             string MaxString = obj2.TestMaximum();
-            Console.WriteLine("Maximum value in string is: " + MaxString);
-            Console.WriteLine("**********************************************");
+            obj2.PrintMax();
+
 
         }
     }
