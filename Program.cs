2c53dbccef261718dbe57e90e2fec7d8515139d1
DAAN <daanmgneni@gmail.com>
2023-04-01 03:22:18 +0530

    Main Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericDemo;
+
+
+namespace GenreicDemo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Generic Program");
+        }
+
+    }
+}
