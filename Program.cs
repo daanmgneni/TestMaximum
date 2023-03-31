24db2a76699bb54d3e8071fff7c5b52ae8f53aaf
DAAN <daanmgneni@gmail.com>
2023-04-01 03:29:45 +0530

    Add:UC1_Program to compare integers

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace GenreicDemo
         public static void Main()
         {
             Console.WriteLine("Welcome to Generic Program");
+            Console.WriteLine("Please Enter the FirstValue:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the SecondValue:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the ThirdValue:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int val = FindMax.MaximumNum(num1, num2, num3);
+            Console.WriteLine("{0} is greater", val);
         }
 
     }
